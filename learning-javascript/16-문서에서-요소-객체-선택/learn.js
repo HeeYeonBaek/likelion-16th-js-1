7f08bfa1bf18dbfa859ed2bd45d4228de1668c7e
yamoo9 <yamoo9@naver.com>
2026-01-14 11:38:14 +0900

    학습: 코드 재사용의 필요성
    - 결론은 함수 (재사용성)
    - 기능이 필요함을 인지
    - 함수를 구현하는 능력

--- learning-javascript/16-문서에서-요소-객체-선택/learn.js
+++ learning-javascript/16-문서에서-요소-객체-선택/learn.js
@@ -52,24 +52,42 @@
 
 // --------------------------------------------------------------------------
 // document.querySelector - 전체 문서 탐색
-// --------------------------------------------------------------------------
-
+// -------------------------------------------------------------------------
 
 // 1. ID 선택자를 사용하여 '#chapter' 요소를 선택하고 콘솔에 출력하세요.
-const chapterElement = document.querySelector('#content')
+let chapterElement = document.getElementById('chapter')
+chapterElement = document.querySelector('#chapter')
 console.log(chapterElement) // Element 또는 null
 
 // 조건 처리 (문서에 요소가 존재하는지 검토)
 // chapterElement || console.warn('#chapter 요소가 문서에 없습니다.')
 // if (!chapterElement) {
-if (chapterElement === null) {
-  console.warn('#chapter 요소가 문서에 없습니다.')
-}
+checkElementWarn(chapterElement, '#chapter')
 
 // 2. 클래스 선택자를 사용하여 '.sr-only' 요소를 선택하고 콘솔에 출력하세요.
+const screenReaderOnlyElement = document.querySelector('.sr-only')
+console.log(screenReaderOnlyElement)
 
+checkElementWarn(screenReaderOnlyElement, '.sr-only')
 
 // 3. 속성 선택자를 사용하여 title에 'Model'이 포함된([title*="Model"]) 요소를 선택하세요.
+const modelElement = document.querySelector('[title*="Model"]')
+console.log(modelElement)
+
+checkElementWarn(modelElement, '[title*="Model"]')
+
+
+// 반복되는 코드를 재사용하기 위해 함수를 작성하기로 했다.
+// 함수(기능)에 걸맞는 이름을 작성하기로 했다.
+// 기능: 문서에 요소가 존재하는지 확인한 후, 존재하지 않을 경우 콘솔에 경고하기로 했다.
+// 목적: 현재 어떤 상황의 문제가 발생했는지 개발자에게 알리고자 한다.
+function checkElementWarn(element, selector) {
+  // 요소가 문서에 존재하는지 검증
+  // 문서에 요소가 없다면? 콘솔에 경고
+  if (element === null) {
+    console.warn(selector + ' 선택자로 문서에서 요소를 찾을 수 없습니다.')
+  }
+}
 
 
 // 설명:
